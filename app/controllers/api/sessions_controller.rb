1049e3df2fbfb989580683635036fa674346754e
jordvnkm <minatogawaj@gmail.com>
2016-11-12 14:48:06 -0800

    setup users and sessions controlelr

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,13 +1,34 @@
 class Api::SessionsController < ApplicationsController
   def show
+    @user = current_user
 
+    if @user
+      render 'api/users/show'
+    else
+      render 'api/shared/error', status 404
+    end
   end
 
   def create
+    @user = Api::User.find_by_credentials(params[:user][:username], params[:user][:password])
 
+    if @user
+      login_user(@user)
+      render 'api/users/show'
+    else
+      @errors = ['invalid username/password']
+      render 'api/shared/error', status 404
+    end
   end
 
   def destroy
-
+    @user = current_user
+    if @user
+      logout_user
+      render 'api/users/show'
+    else
+      @errors = ['nobody logged in']
+      render 'api/shared/error'
+    end
   end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,16 +11,40 @@ class Api::UsersController < ApplicationsController
   end
 
   def show
-
+    @user = current_user
+    if @user
+      render 'api/users/show'
+    else
+      render json: nil, status: 404
+    end
   end
 
   def create
+    @user = Api::User.new(user_params)
 
+    if @user.save
+      login_user(@user)
+      render 'api/users/show'
+    else
+      @errors = @user.errors.full_messages
+      render 'api/shared/error', status: 422
+    end
   end
 
   def update
+    @user = Api::User.find(params[:id])
+    @user.update(user_params)
 
+    if @user.save
+      render 'api/users/show'
+    else
+      @errors = @user.errors.full_messages
+      render 'api/shared/error', status: 422
+    end
   end
 
-  
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :profile_img_url, :email)
+  end
 end
